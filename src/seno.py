abdb65dc24baa8eaee8dc3b50fc1e443b66cf68d
Gorbyy <gonga_nunes@hotmail.com>
2016-06-08 01:36:58 +0100

    cmakelists e xml alterados para criar ficheiros .msg. action server e client a funcionar

--- src/seno.py
+++ src/seno.py
@@ -1,6 +1,5 @@
 #! /usr/bin/env python
 
-import turtle
 import numpy as np # to work with numerical data efficiently
 import matplotlib.pyplot as plt
 
@@ -10,13 +9,14 @@ limitmin = -amplitude/2
 
 position = 50
 
+
+
+
 fs = 500 # sample rate 
 f = 3 # the frequency of the signal
 
-
 x = np.arange(fs) # the points on the x axis for plotting
 # compute the value (amplitude) of the sin wave at the for each sample
-
 y = np.sin(2*np.pi*f * x / fs)*(amplitude/2) + position
 
 for i in range(len(y)):
@@ -27,6 +27,7 @@ for i in range(len(y)):
 
 
 plt.plot(x, y)
+plt.plot(x, -y)
 plt.xlabel('sample(n)')
 plt.ylabel('angle')
 plt.show()
--- src/teste_server.py
+++ src/teste_server.py
@@ -2,11 +2,10 @@
 
 import roslib; roslib.load_manifest('behavioural_state_machine')
 import rospy
-
 import actionlib
-
 import behavioural_state_machine.msg
- 
+import numpy as np # to work with numerical data efficiently
+import matplotlib.pyplot as plt
 
 class TesteAction(object):
   # create messages that are used to publish feedback/result
@@ -19,7 +18,7 @@ class TesteAction(object):
                                             behavioural_state_machine.msg.TesteAction, 
                                             execute_cb=self.execute_cb, auto_start = False)
     self._as.start()
-    
+
   def execute_cb(self, goal):
     # helper variables
     r = rospy.Rate(1)
@@ -27,28 +26,42 @@ class TesteAction(object):
     self._feedback.feed = 0
     
     # publish info to the console for the user
-    rospy.loginfo('action_name: %s, goal.limits: %i, feedback.feed: %i' % (self._action_name, goal.limits, self._feedback.feed))
-    
+    print('goal.amplitude: {}, goal.position: {}'.format(goal.amplitude, goal.position))
 
 
-    # start executing the action
-    for i in xrange(1, goal.limits):
-      # check that preempt has not been requested by the client
-      if self._as.is_preempt_requested():
-        rospy.loginfo('%s: Preempted' % self._action_name)
-        self._as.set_preempted()
-        success = False
-        break
+    self._feedback.feed = 1;      
 
-      self._feedback.feed = i;
-      print 'feedback: %i' %self._feedback.feed
-      
-      # this step is not necessary, the sequence is computed at 1 Hz for demonstration purposes
-      r.sleep()
+    fs = 500 # sample rate 
+    f = 3 # the frequency of the signal
+    limitmax = goal.amplitude/2
+    limitmin = -goal.amplitude/2
+
+    x = np.arange(fs) # the points on the x axis for plotting
+    # compute the value (amplitude) of the sin wave at the for each sample
+    y = np.sin(2*np.pi*f * x / fs)*(goal.amplitude/2) + goal.position
+
+    for i in range(len(y)):
+      if y[i] > limitmax:
+        y[i] = limitmax
+      elif y[i] < limitmin:
+        y[i] = limitmin
+
+    # check that preempt has not been requested by the client
+    if self._as.is_preempt_requested():
+      rospy.loginfo('%s: Preempted' % self._action_name)
+      self._as.set_preempted()
+      success = False
 
     if success:
-      self._result.res = self._feedback.feed
-      rospy.loginfo('%s succeeded, result: %i' % (self._action_name, self._result.res))
+      self._result.x = x
+      self._result.y = y
+      self._result.z = -y
+      print('%s: succeeded' % self._action_name)
+      plt.plot(self._result.x, self._result.y)
+      plt.plot(self._result.x, self._result.z)
+      plt.xlabel('sample(n)')
+      plt.ylabel('angle')
+      plt.show()
       self._as.set_succeeded(self._result)
       
 if __name__ == '__main__':
